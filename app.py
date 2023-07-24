704f99fa3b5f3d763b0b718b2fcf3151f5f5bdd6
jasredyy5 <jassureddy5@gmail.com>
2023-07-24 12:38:42 -0400

    Chnages to Response

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Web App devloped in Pyhton and Flask"
+    return "Hello, this is a sample Web App developed in Pyhton and Flask"
 
